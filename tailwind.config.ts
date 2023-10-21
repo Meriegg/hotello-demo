4b033d6107d209b57c98d86d2a139d0c3a29ec08
Meriegg <ya.boybeats1234@gmail.com>
2023-10-21 12:50:22 +0300

    Added: colors in globals.css and tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,21 @@ export default {
       fontFamily: {
         sans: ["var(--font-sans)", ...fontFamily.sans],
       },
+      colors: {
+        "neutral": {
+          '50': 'var(--neutral-50)',
+          '100': 'var(--neutral-100)',
+          '200': 'var(--neutral-200)',
+          '700': 'var(--neutral-700)',
+          '900': 'var(--neutral-900)',
+        },
+        "red": {
+          '400': 'var(--red-400)'
+        },
+        "yellow": {
+          '500': 'var(--yellow-500)'
+        }
+      }
     },
   },
   plugins: [],
