dcc685b9feee5d57a838a8c1c0f3881493ec0895
Meriegg <ya.boybeats1234@gmail.com>
2024-01-20 22:49:27 +0200

    Resolved revert conflicts

--- src/app/account/dashboard/page.tsx
+++ src/app/account/dashboard/page.tsx
@@ -2,21 +2,8 @@ import { ChangeAccountDetails } from "../components/change-account-details";
 import { SecurityOptions } from "../components/security-options";
 import { getSession } from "../../utils/get-page-session";
 
-<<<<<<< HEAD
-const Page = () => {
-  const { data: currentSession, ...currentSessionInfo } = useSession();
-
-  if (currentSessionInfo.isLoading) {
-    return <Loader label="Fetching account data" />
-  }
-
-  if (currentSessionInfo.isError || !currentSession) {
-    return <p className="text-neutral-700 text-xs text-center w-full">You are not logged in.</p>
-  }
-=======
 const Page = async () => {
   const currentSession = await getSession();
->>>>>>> parent of 7d86e3b (Temporarily fixed auth checking problem)
 
   return (
     <div>
